840da13adde8fcd9c51e69436c852383652d56be
MaryVazhnik <36933552+MaryVazhnik@users.noreply.github.com>
2018-12-02 17:10:22 +0200

    Update XmlParser.cs

--- DEV_8/DEV_8/XmlParser.cs
+++ DEV_8/DEV_8/XmlParser.cs
@@ -12,45 +12,54 @@ namespace DEV_8
         List<Car> carList = new List<Car>();
         List<Truck> truckList = new List<Truck>();
 
-        public List<Vehicle> XmlParser()
+        private List<Vehicle> XmlParser(List<String> args)
         {
-            XDocument xdoc = XDocument.Load("cars.xml");
-            foreach (XElement phoneElement in xdoc.Element("cars").Elements("car"))
-            {
-                XElement modelElement = phoneElement.Element("model");
-                XElement brandElement = phoneElement.Element("brand");
-                XElement quantityElement = phoneElement.Element("amount");
-                XElement priceElement = phoneElement.Element("price");
+			for (int i = 0; i < 2; i++)
+			{
+				XDocument xdoc = XDocument.Load(args.ElementAt(0));
+				if (xdoc.Element("cars")
+				{
+					foreach (XElement carElement in xdoc.Element("cars").Elements("car"))
+					{
+						XElement modelElement = phoneElement.Element("model");
+						XElement brandElement = phoneElement.Element("brand");
+						XElement quantityElement = phoneElement.Element("amount");
+						XElement priceElement = phoneElement.Element("price");
 
-                if (modelElement != null && brandElement != null && quantityElement != null && priceElement != null)
-                {
-                    Console.WriteLine("Model: {0}", modelElement.Value);
-                    Console.WriteLine("Price of one: {0}", priceElement.Value);
-                    Console.WriteLine("Quantity: {0}", quantityElement.Value);
-                    Console.WriteLine("Brand: {0}", brandElement.Value);
+						if (modelElement != null && brandElement != null && quantityElement != null && priceElement != null)
+						{
+							Console.WriteLine("Model: ", modelElement.Value);
+							Console.WriteLine("Price of one: ", priceElement.Value);
+							Console.WriteLine("Quantity: ", quantityElement.Value);
+							Console.WriteLine("Brand: ", brandElement.Value);
 
-                    carList.Add(new Car(brandElement.Value, modelElement.Value, Convert.ToInt32(priceElement.Value), Convert.ToInt32(quantityElement.Value)));
-                }
-            }
-            XDocument xdocument = XDocument.Load("trucks.xml");
-            foreach (XElement phoneElement in xdoc.Element("trucks").Elements("truck"))
-            {
-                XElement modelElement = phoneElement.Element("model");
-                XElement brandElement = phoneElement.Element("brand");
-                XElement quantityElement = phoneElement.Element("amount");
-                XElement priceElement = phoneElement.Element("price");
+							carList.Add(new Car(brandElement.Value, modelElement.Value, Convert.ToInt32(priceElement.Value), Convert.ToInt32(quantityElement.Value)));
+						}
+					}
+					return carList;
+				}
+				else if (xdoc.Element("trucks")
+				{
+					foreach (XElement truckElement in xdoc.Element("trucks").Elements("truck"))
+					{
+						XElement modelElement = phoneElement.Element("model");
+						XElement brandElement = phoneElement.Element("brand");
+						XElement quantityElement = phoneElement.Element("amount");
+						XElement priceElement = phoneElement.Element("price");
 
-                if (modelElement != null && brandElement != null && quantityElement != null && priceElement != null)
-                {
-                    Console.WriteLine("Model: {0}", modelElement.Value);
-                    Console.WriteLine("Price of one: {0}", priceElement.Value);
-                    Console.WriteLine("Quantity: {0}", quantityElement.Value);
-                    Console.WriteLine("Brand: {0}", brandElement.Value);
+						if (modelElement != null && brandElement != null && quantityElement != null && priceElement != null)
+						{
+							Console.WriteLine("Model: ", modelElement.Value);
+							Console.WriteLine("Price of one: ", priceElement.Value);
+							Console.WriteLine("Quantity: ", quantityElement.Value);
+							Console.WriteLine("Brand: ", brandElement.Value);
 
-                    truckList.Add(new Truck(brandElement.Value, modelElement.Value, Convert.ToInt32(priceElement.Value), Convert.ToInt32(quantityElement.Value)));
-                }
-            }
-            return null;
+							truckList.Add(new Truck(brandElement.Value, modelElement.Value, Convert.ToInt32(priceElement.Value), Convert.ToInt32(quantityElement.Value)));
+						}
+					}
+					return truckList;
+				}
+			}
         }
     }
 }
