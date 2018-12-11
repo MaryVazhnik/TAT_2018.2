390e77d5ae7066304e37006fd199369954d99e8b
Maryia Vazhnik <donmarino79@gmail.com>
2018-12-11 17:41:51 +0200

    DEV-9

---
+++ DEV-9/SearchOnVK/SearchOnVK/SearchElements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+
+namespace SearchOnVK
+{
+    class SearchElements
+    {
+        IWebDriver driver = new ChromeDriver();
+        public void Info(string login, string password)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "https://vk.com/";
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.XPath("//*[@id='index_email']")).SendKeys(login);
+            driver.FindElement(By.XPath("//*[@id='index_pass']")).SendKeys(password + Keys.Enter);
+
+            driver.FindElement(By.XPath("//li[@id='l_msg']")).Click();
+            driver.FindElement(By.XPath("//a[@id='ui_rmenu_unread']")).Click();
+
+            IWebElement name = driver.FindElement(By.XPath("//span[@class= '_im_dialog_link']"));
+            IWebElement message = driver.FindElement(By.XPath("//div[@class= 'nim-dialog--cw']"));
+            IWebElement date = driver.FindElement((By.XPath("//div[@class= 'nim-dialog--date_im_dialog_date']")));
+            Console.WriteLine(name.Text + "   " + date.Text);
+            Console.WriteLine(message.Text);
+        }
+    }
+}
